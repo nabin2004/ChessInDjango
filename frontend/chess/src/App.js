ef97733fd1963f4f11d96bd271d3322bc02dbb66
Nabin Oli <olinabin2004@gmail.com>
2024-10-27 11:59:53 +0545

    Added the new pieeces

--- frontend/chess/src/App.js
+++ frontend/chess/src/App.js
@@ -1,12 +1,27 @@
 // import './App.css';
 import { Chessboard } from "react-chessboard";
 import { Chess } from "chess.js";
-import { useState } from "react";
+import { useState, useMemo } from "react";
+
 
 function App() {
   const [game, setGame] = useState(new Chess());
   const [position, setPosition] = useState(game.fen());
-
+  const pieces = ["wP", "wN", "wB", "wR", "wQ", "wK", "bP", "bN", "bB", "bR", "bQ", "bK"];
+  const customPieces = useMemo(() => {
+    const pieceComponents = {};
+    pieces.forEach(piece => {
+      pieceComponents[piece] = ({
+        squareWidth
+      }) => <div style={{
+        width: squareWidth,
+        height: squareWidth,
+        backgroundImage: `url(${process.env.PUBLIC_URL}/pieces/${piece}.png)`,
+        backgroundSize: "100%"
+      }} />;
+    });
+    return pieceComponents;
+  }, []);
   // Function to handle moves
   function onDrop(sourceSquare, targetSquare) {
     try {
@@ -35,11 +50,14 @@ function App() {
   return (
     <div className="App h-screen flex justify-center items-center">
       <div className="w-full max-w-md flex justify-center">
-        <Chessboard
-          id="BasicBoard"
-          position={position}
-          onPieceDrop={onDrop} // This handles move validation
-        />
+      <Chessboard id="StyledBoard" boardOrientation="black" position={game.fen()} onPieceDrop={onDrop} customBoardStyle={{
+      borderRadius: "4px",
+      boxShadow: "0 2px 10px rgba(0, 0, 0, 0.5)"
+    }} customDarkSquareStyle={{
+      backgroundColor: "#779952"
+    }} customLightSquareStyle={{
+      backgroundColor: "#edeed1"
+    }} customPieces={customPieces} />
       </div>
     </div>
   );
